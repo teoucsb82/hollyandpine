fcf9451ca88c4b4e9f19d55b709fa67567e5d179
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-11-19 00:19:38 -0800

    Added my account page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users
   root 'home#index'
   resources :about, only: [:index]
   resources :contact, only: [:index]
   resources :home, only: [:index]
+  resources :users, path: 'my-account', only: [] do
+    get '/', to: :show, on: :collection
+  end
 end
