09ef3026605830e5ae44c723f424a2e5f3655392
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-11-20 15:43:42 -0800

    Added images backstop

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -6,5 +6,16 @@ class ServicesController < ApplicationController
 
   def show
     @service = services.detect { |service| service.id == params[:id] }
+    @images = @service.images || default_service_images
+  end
+
+  private
+
+  def default_service_images
+    [
+      'product-slide1.jpg',
+      'product-slide2.jpg',
+      'product-slide3.jpg'
+    ]
   end
 end
