0242fcf77740410ffd68315cd4c92f32650bd6bd
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-11-22 09:24:12 -0800

    Removed admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
-
-  namespace :admin do
-    get '/', to: 'admin#index'
-    resources :users, only: [:index, :show]
-  end
-
   resources :about, only: [:index]
   resources :contact, only: [:index]
   resources :home, only: [:index]
