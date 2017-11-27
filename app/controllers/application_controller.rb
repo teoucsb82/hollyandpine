4a4ff6bb9821111a92e7b822390fa50957a9cba3
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-11-26 19:08:48 -0800

    Added meta description and keywords

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_metatags
+
+  def set_metatags
+    default_description = 'A lifestyle company providing luxury gift styling and wrapping.'
+    default_keywords = 'Toronto, Canada, Wrapping, Present, Gift, Christmas, Lifestyle, Stationary, Paper'
+    set_meta_tags(
+      description: @meta_description || default_description,
+      keywords: @meta_keywords || default_keywords 
+    )
+  end
 end
