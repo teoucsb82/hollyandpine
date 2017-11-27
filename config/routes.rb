4d1a3caa173c24897f8c7f3972b52a9e5e9436a9
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-11-26 19:49:48 -0800

    Updated Terms Page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :faq, only: [:index]
   resources :home, only: [:index]
   resources :services, only: [:index, :show]
+  resources :terms_and_conditions, path: 'terms-and-conditions', only: [:index]
   resources :users, path: 'my-account', only: [:update] do
     get '/', to: :show, on: :collection
   end
