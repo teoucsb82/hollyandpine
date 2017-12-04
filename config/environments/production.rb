31eb67a5d248123535f5dcd3261d0466d6b1e5aa
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-12-03 17:50:37 -0800

    Updated host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
+
+  config.action_mailer.default_url_options = { 
+    host: 'www.hollyandpine.ca' 
+  }
 end
