89690a15f31c3fab970722643369baa068880283
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-12-05 11:57:55 -0800

    Added custom errors routes for 404 and 500

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
   root 'home#index'
   devise_for :users
   namespace :admin do
     resources :landing_pages, path: 'blog', except: [:show]
-    resources :orders, only: [:show, :index, :destroy]
+    resources :orders, only: %i[show index destroy]
   end
   resources :about, only: [:index]
   resources :contact, only: [:index]
   resources :faq, only: [:index]
   resources :home, only: [:index]
   resources :landing_pages, path: 'blog', only: [:show]
-  resources :orders, only: [:new, :create, :show]
-  resources :services, only: [:index, :show]
+  resources :orders, only: %i[new create show]
+  resources :services, only: %i[index show]
   resources :terms_and_conditions, path: 'terms-and-conditions', only: [:index]
   resources :users, path: 'my-account', only: [:update] do
     get '/', to: :show, on: :collection
