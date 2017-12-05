9739941248279d5a93636d97fcb0b67bf29f2ec3
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-12-05 12:06:34 -0800

    Undid rubocop styling on routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,15 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
     resources :landing_pages, path: 'blog', except: [:show]
-    resources :orders, only: %i[show index destroy]
+    resources :orders, only: [:show, :index, :destroy]
   end
   resources :about, only: [:index]
   resources :contact, only: [:index]
   resources :faq, only: [:index]
   resources :home, only: [:index]
   resources :landing_pages, path: 'blog', only: [:show]
-  resources :orders, only: %i[new create show]
-  resources :services, only: %i[index show]
+  resources :orders, only: [:new, :create, :show]
+  resources :services, only: [:index, :show]
   resources :terms_and_conditions, path: 'terms-and-conditions', only: [:index]
   resources :users, path: 'my-account', only: [:update] do
     get '/', to: :show, on: :collection
