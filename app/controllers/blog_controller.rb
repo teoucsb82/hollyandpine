ed7423a5b28d9265c0312d8135da2230a84a9be6
Katharine Sellers <katharinesellers@Katharines-MacBook-Pro.local>
2017-12-17 17:02:41 -0500

    Added rubocop

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,11 +1,11 @@
 class BlogController < ApplicationController
-	def index
-		@blogs = LandingPage.all
-	end
+  def index
+    @blogs = LandingPage.all
+  end
 
-	def show
-		@landing_page = LandingPage.friendly.find(params[:id])
-		@recent_landing_pages = LandingPage.all.last(5)
-		render 'landing_pages/show'
-	end
+  def show
+    @landing_page = LandingPage.friendly.find(params[:id])
+    @recent_landing_pages = LandingPage.all.last(5)
+    render 'landing_pages/show'
+  end
 end
